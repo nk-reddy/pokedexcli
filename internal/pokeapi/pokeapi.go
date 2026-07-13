c55e84d710a79bd665daed2728ae26c3cd0097da
Nikhil Reddy <nikhilreddy6179@gmail.com>
2026-07-13 12:05:44 -0400

    adding caching

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -1,5 +1,13 @@
 package pokeapi
 
+import (
+	"time"
+
+	"github.com/nk-reddy/pokedexcli/internal/pokecache"
+)
+
 const (
 	baseURL = "https://pokeapi.co/api/v2"
 )
+
+var cache = pokecache.NewCache(5 * time.Second)
